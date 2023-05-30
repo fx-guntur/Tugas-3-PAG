a1351ab6500c53c1805a81e400906a605130314b
Fx. Guntur <109921920+fx-guntur@users.noreply.github.com>
2023-05-30 21:58:24 +0700

    Add files via upload

---
+++ tugas3_PAG/src/main/java/com/mycompany/tugas3_pag/DBConnection.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tugas3_pag;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author ACER
+ */
+public class DBConnection {
+    private final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private final String DB_URL = "jdbc:mysql://localhost:3306/tugas3_pag";
+    private final String USER = "root";
+    private final String PASS = "";
+
+    public Connection conn;
+    public void connection() throws ClassNotFoundException, SQLException {
+        DriverManager.registerDriver(new com.mysql.jdbc.Driver ());
+        conn = DriverManager.getConnection(DB_URL, USER, PASS);
+    }
+    
+    public Statement openConnection() throws SQLException{
+        return conn.createStatement();
+    }
+}
